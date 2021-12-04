351966ae01edd759027f3ee31397505d6d26b9e5
plmqazoknijb <w2033@e-mirim.hs.kr>
2021-12-04 23:29:19 +0900

    Edit ShootingGame.java ANd Add Main.java (화면구성 1)

--- src/ShootingGame.java
+++ src/ShootingGame.java
@@ -1,2 +1,21 @@
-public class ShootingGame {
+import javax.swing.*;
+import java.awt.*;
+
+public class ShootingGame extends JFrame {
+    //더블 버퍼
+    public Image bufferImage;
+    private Graphics screenGraphic;
+
+
+
+    public ShootingGame(){
+        setTitle("슈팅게임");
+        setUndecorated(true);   //GUI 테투리 없게
+        setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setLayout(null);
+    }
 }
