7d753ec21d78399f1347007dc658a5b1207e9763
plmqazoknijb <w2033@e-mirim.hs.kr>
2021-12-05 00:10:16 +0900

    Edit ShootingGame.java (Main, loading, gameStart)

--- src/ShootingGame.java
+++ src/ShootingGame.java
@@ -1,12 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ShootingGame extends JFrame {
     //더블 버퍼
     public Image bufferImage;
     private Graphics screenGraphic;
 
+    private Image mainScreen = new ImageIcon("src/images/main_screen.png").getImage();  //Main화면
+    private Image loadingScreen = new ImageIcon("src/images/loading_screen.png").getImage();    //loading화면
+    private Image gameScreen = new ImageIcon("src/images/game_screen.png").getImage();  //game화면
 
+    private boolean isMainScreen, isLoadingScreen, isGameScreen;
 
     public ShootingGame(){
         setTitle("슈팅게임");
@@ -17,5 +25,67 @@ public class ShootingGame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setLayout(null);
+
+        init();
+    }
+
+    private void init(){    //초기화
+        isMainScreen = true;
+        isLoadingScreen = false;
+        isGameScreen = false;
+
+        addKeyListener(new KeyListener());
+    }
+
+    public void paint(Graphics g){  //메인화면 출력
+        bufferImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+        screenGraphic = bufferImage.getGraphics();
+        screenDraw(screenGraphic);
+        g.drawImage(bufferImage,0,0,null);
+    }
+
+    private void gameStart(){
+        isMainScreen = false;
+        isLoadingScreen = true;
+
+        Timer loadingTimer = new Timer();
+        TimerTask loadingTask = new TimerTask() {
+            @Override
+            public void run() {
+                isLoadingScreen = false;
+                isGameScreen = true;
+            }
+        };
+        loadingTimer.schedule(loadingTask,3000);    //로딩화면 --3초--> 게임화면
+    }
+
+    public void screenDraw(Graphics g){ //필요한 요소 그려줌
+        if(isMainScreen){
+            g.drawImage(mainScreen,0,0,null);
+        }
+        if(isLoadingScreen){
+            g.drawImage(loadingScreen,0,0,null);
+        }
+        if(isGameScreen){
+            g.drawImage(gameScreen,0,0,null);
+        }
+        this.repaint();
+    }
+
+
+    class KeyListener extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()){
+                case KeyEvent.VK_ENTER:     //Enter누르면 게임방법화면으로
+                    if (isMainScreen){
+                        gameStart();
+                    }
+                    break;
+                case KeyEvent.VK_ESCAPE:     //Esc누르면 종료
+                    System.exit(0);
+                    break;
+            }
+        }
     }
 }
